d992ea76cb021b3d3a4b2fdbc1ef446b607ccaf0
EhisEA <emmanueleayemere@gmail.com>
2022-07-02 13:17:31 +0100

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,22 +13,22 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		j = i + 1;	
-		 for (; j < 9; j++)
-		{	
+		j = i + 1;
+		for (; j < 9; j++)
+		{
 			k = j + 1;
-			 for (; k < 10; k++)
-               		 {
-                       		 putchar(i + '0');
-                       		 putchar(j + '0');
-                       		 putchar(k + '0');
-                       		 if (i != 7)
-                       		 {
-                               		putchar(',');
-                               		putchar(' ');
-                        	}
+			for (; k < 10; k++)
+               		{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-                }
+		}
 	}
 	putchar('\n');
 

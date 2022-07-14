299fbeeb87ee44e79c25dfb3499a731c223c456a
EhisEA <emmanueleayemere@gmail.com>
2022-07-15 00:14:04 +0100

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+
+	char start = 'a';
+	char end = 'z';
+	
+	++end;
+	while (start != end) {
+		putchar(start);	
+		++start;
+	}
+	putchar('\n');
+	return (0);
+}
